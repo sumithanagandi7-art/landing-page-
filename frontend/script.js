be5448e3761adebc90b8b2b22e5a9045d6ddffdd
sumithanagandi7-art <sumithanagandi7@example.com>
2026-05-10 16:36:44 +0530

    fixed popup

--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,9 @@
-function scrollToContact() {
-    document.getElementById("contact").scrollIntoView();
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
 
-document.getElementById("contactForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
-
-    const data = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    };
-
-    const res = await fetch("http://localhost:5000/contact", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
-
-    const result = await res.json();
-    alert(result.message);
-    document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     alert("Form submitted successfully!");
-
-    form.reset();
   });
-});
 });
